8058a37d28ce175302081c094c0078d1c2782774
robertapdm <robertafarmaco@gmail.com>
2024-03-23 16:36:19 -0300

    adding links to the social img

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -19,8 +19,8 @@ function Footer(){
                     <S.A href=""><li>Personagens</li></S.A>
                 </S.Ul>
             <S.DivSocial className="sociais">
-                <S.ImgSocial src={Github} alt="" />
-                <S.ImgSocial src={Linkedin} alt="" />
+                <a href="https://github.com/robertapdm" target="_blank"><S.ImgSocial src={Github} alt="Logo do GitHub" /></a>
+                <a href="https://www.linkedin.com/in/roberta-mota-20937b295/" target="_blank"><S.ImgSocial src={Linkedin} alt="Logo do Linkedin" /></a>
             </S.DivSocial>
             </S.Div>
             <S.DivAutoral>
